71585c39a6d16bd379f568639508289326710efb
Roman Bakaldin <gbakaldin@gmail.com>
2025-10-08 15:54:24 +0300

    Reformat code

--- reflect/src/main/java/com/github/romanqed/unpackr/reflect/Accessors.java
+++ reflect/src/main/java/com/github/romanqed/unpackr/reflect/Accessors.java
@@ -4,7 +4,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
-@SuppressWarnings("rawtypes")
 final class Accessors {
     private Accessors() {
     }
